4a1d6b54b398383e28609c8f02d7d6138dda365e
Pa1nless <72404226+Pa1nless@users.noreply.github.com>
2021-09-29 22:41:54 -0700

    Update sv.lua

--- server/sv.lua
+++ server/sv.lua
@@ -6,6 +6,7 @@ end
 ESX.RegisterServerCallback("p1n-info:getData", function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
     cb({
+        job = xPlayer.getJob().label,
         name = xPlayer.getName(),
         cash = xPlayer.getAccount("money").money,
         bank = xPlayer.getAccount("bank").money,
